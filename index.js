db7e899aa67b751f1f0d948467ef23c283312929
Darren Finch <finchdarren25@gmail.com>
2022-07-27 09:04:11 -0500

    Add navigation

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-import "bootstrap";
+import * as $ from "./node_modules/jquery/dist/jquery.js";
+import * as bootstrap from "./node_modules/bootstrap/dist/js/bootstrap.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const navbarToggler = document.querySelector(".navbar-toggler");
+
+	navbarToggler.addEventListener("click", () => {
+        if (navbarToggler.classList.contains("active")) {
+            navbarToggler.classList.remove("active");
+        } else {
+            navbarToggler.classList.add("active");
+        }
+	});
+});
